d813fc4dad0df197d5b1d31ee267e190e1290505
Sayanjit Mukherjee <silverfangthegem@gmail.com>
2025-07-06 23:51:22 +0530

    add: Implement file I/O for shares.
    Signed-off-by: Sayanjit Mukherjee <silverfangthegem@gmail.com>

--- internal/util/io.go
+++ internal/util/io.go
@@ -1,1 +1,27 @@
 package util
+
+
+import (
+    "encoding/json"
+    "github.com/Thedrogon/S4/pkg/types"
+    "os"
+)
+
+// SaveShares saves shares to a JSON file
+func SaveShares(shares []types.Share, filename string) error {
+    data, err := json.Marshal(shares)
+    if err != nil {
+        return err
+    }
+    return os.WriteFile(filename, data, 0644)
+}
+
+// LoadShares loads shares from a JSON file
+func LoadShares(filename string) ([]types.Share, error) {
+    data, err := os.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    var shares []types.Share
+    return shares, json.Unmarshal(data, &shares)
+}
